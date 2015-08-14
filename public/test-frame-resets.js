a0020d7cb33bcd386c10d8c82eb5e6e854fb49d3
mikegai <mike.d.gai@gmail.com>
2015-08-14 11:51:21 -0700

    Fixed core logic bug

--- public/test-frame-resets.js
+++ public/test-frame-resets.js
@@ -1,13 +1,17 @@
-var svgns = "http://www.w3.org/2000/svg";
-var framesInStory = 10;
 var numberOfUpdates = 10;
 var sampleEvery = 1;
 var accumulated = 0;
 var samples = [];
-var removeClipping = false;
+var removeClipping = true;
 var frameIds = ["eternal", "launch", "300e6bea-cc3c-46eb-a65e-010d943f2bbb", "e2354a77-d92b-489f-8bc3-fc7ffacb1c73", "a06e8505-999d-4ce2-a117-f92e83618ee3", "4caa2135-46a0-4bd8-a3f8-145f58cee7db", "0fefc2f2-6368-45a8-9f59-c849136fa1b3", "a30130bf-e66d-467d-867f-b2129f24125a", "234467c0-755a-4c41-8d0b-2b3182fde130", "0f3f82e0-d739-4c81-afe3-7b449e0e0178"]
+var framesInStory = frameIds.length;
+var frameNodes = _.map(frameIds,function(frameId){
+    return  document.querySelector('.frame-thumbnails').querySelector('.frame-thumbnail[data-id="'+frameId+'"]' );
+});
+console.log(frameNodes);
 
-var frameHolderTemplate=document.getElementById('frame-holder-template');
+
+//PROCESS TEMPLATE
 var frameTemplate =document.getElementById('frame-template');
 //remove from template
 var clipPaths = frameTemplate.content.querySelectorAll('clipPath');
@@ -17,7 +21,7 @@ var clippedById = _.groupBy(clipped,function(el){
     var c = el.getAttribute('clip-path');
     return c.substring(5, c.length-1)});
 
-console.log(clipPathsById, clippedById,'clipped groups')
+console.log(clipPathsById, clippedById,'clipped groups');
 
 if (removeClipping) {
     _.each(clipPaths, function (c) {
@@ -25,40 +29,38 @@ if (removeClipping) {
     });
 }
 
-document.body.innerHTML = '<svg id="frame-thumbnails" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100%" height="100%"></svg>'
-
-
-var frameThumbnails = document.getElementById('frame-thumbnails');
-
-buildFrameHolders();
-
-var i = 0;
+//BEGIN TESTING
+var updateIndex = 0;
 var intervalId = setInterval(function () {
+    console.log('\n');
     console.log ('updating ' + framesInStory + ' frames');
     //update all the frames by clearing and setting them...
-    for (var j=0;j<framesInStory;j++){
+    for (var frameIndex=0;frameIndex<framesInStory;frameIndex++){
         var startTime = Date.now();
-        clearFrame(j);
+        console.log('Clearing',frameIndex);
+        clearFrame(frameIndex);
         var endTime = Date.now();
         accumulated += endTime - startTime;
-        setFrame(j);
+        setFrame(frameIndex);
     }
-    i++;
+    updateIndex++;
 
 
-    if (i % sampleEvery === 0) {
-        console.log('update cycle', i, ': ',accumulated / sampleEvery / framesInStory, 'ms per frame');
+    if (updateIndex % sampleEvery === 0) {
+        console.log('update cycle', updateIndex, ': ',accumulated / sampleEvery / framesInStory, 'ms per frame');
         samples.push(accumulated / sampleEvery);
         accumulated = 0;
     }
     console.log ('FINISHED updating ' + framesInStory + ' frames');
-    if (i >= numberOfUpdates) {
+    if (updateIndex >= numberOfUpdates) {
         console.log('DONE!');
         console.log(samples);
         clearInterval(intervalId);
     }
-}, 200);
+}, 2000);
 
+
+//HELPERS
 var offset = 0;
 function generateDeviceNode(shuffleClipPaths){
     //return frameTemplate.cloneNode(true).querySelector('.widget-device');
@@ -74,29 +76,20 @@ function generateDeviceNode(shuffleClipPaths){
     return document.importNode(frameTemplate.content,true).querySelector('.widget-device');
 }
 
-function setFrame(j){
-    var frame=document.getElementById('frame-'+j);
-    if (!frame)return;
+function getFrame(i){
+    return frameNodes[i];
+}
+
+function setFrame(i){
+    var frame = getFrame(i);
     var deviceNode = generateDeviceNode(true);
     var contentNode = frame.querySelector('.frame-thumbnail > .frame-body > .content');
     contentNode.appendChild(deviceNode);
 }
-function clearFrame(j) {
-    var frame=document.getElementById('frame-'+j);
+function clearFrame(i) {
+    var frame = getFrame(i);
     if (!frame)return;
     var device = frame.querySelector('.frame-thumbnail > .frame-body > .content > *');
     if (!device) return;
     device.remove(device.parentNode);
-}
-
-//one time
-function buildFrameHolders() {
-    for (var j = 0; j < framesInStory; j++) {
-        //apparently we are supposed to write to template first? weird...
-        var t = frameHolderTemplate.content;
-        t.querySelector('.frame-thumbnail').id='frame-'+j;
-        t.querySelector('.frame-body').setAttribute('transform','translate(' + j*80 + ',0)');
-        var holderNode = document.importNode(t,true);
-        frameThumbnails.appendChild(holderNode.querySelector('.frame-thumbnail'));
-    }
 }
