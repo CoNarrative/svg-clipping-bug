0299a1f2ad6fc330b3c4085e536534eec4022737
mikegai <mike.d.gai@gmail.com>
2015-08-14 19:44:40 -0700

    Refactored to pure.html

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ app.get('/', function (req, res) {
 app.get('/simple', function (req, res) {
     res.render('simple.hbs');
 });
-app.get('/pure',function(req,res){
-    res.render('puretest.hbs');
-})
 
 
 
